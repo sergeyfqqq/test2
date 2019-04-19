6c0f115ab6ce4f6e9c210e72a151f7de420aed99
Sergey <sergeyfqqq@gmail.com>
2019-04-19 12:36:45 +0300

    this is our first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict'
+let i = 10,
+    d = 10;
+
+console.log(i++);
+console.log(d--);
+console.log(5%2);
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = true;
+    
+console.log(isChecked || isClose);
+
+console.log(!isChecked);
+
+a = 11;
+
+console.log(a);
+    
