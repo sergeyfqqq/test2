578c136d36ef69575ea3de72eac79ea8b80ec917
Sergey <sergeyfqqq@gmail.com>
2019-04-22 14:31:25 +0300

    growing up withs git

--- js/script.js
+++ js/script.js
@@ -16,7 +16,39 @@ console.log(isChecked || isClose);
 
 console.log(!isChecked);
 
-a = 11;
-
-console.log(a);
+if (4 + 4 == 8) {
+    console.log('ok');
+} else {
+    console.log('not ok');
+};
     
+
+let num = 50;
+
+if (num < 49) {
+    console.log('smaller than 49');
+} else if (num > 100) {
+    console.log('more than 100');
+} else {
+    console.log('ok');
+};
+
+(num == 50) ? console.log('ok') : console.log('not ok');
+
+switch (num) {
+    case num < 49:
+    console.log('smaller than 49');
+    break;
+    case num < 100:
+    console.log('more than 100');
+    break;
+    case num < 80:
+    console.log('more than 80');
+    break;
+    case 50:
+    console.log('equal 50');
+    break;
+    default: 
+    console.log('smth wrong');
+    break;
+};
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,20 @@
+let num = 50;
+
+
+switch (num) {
+    case num < 49:
+    console.log('smaller than 49');
+    break;
+    case num < 100:
+    console.log('more than 100');
+    break;
+    case num < 80:
+    console.log('more than 80');
+    break;
+    case 50:
+    console.log('equal 50');
+    break;
+    default: 
+    console.log('smth wrong');
+    break;
+};
