68bd398af584947522205c3bdf851f484fcbf39e
Sergey <sergeyfqqq@gmail.com>
2019-04-23 15:44:58 +0300

    again done

--- js/script.js
+++ js/script.js
@@ -1,85 +1,122 @@
-'use strict'
-let i = 10,
-    d = 10;
+// 'use strict'
+// let i = 10,
+//     d = 10;
 
-console.log(i++);
-console.log(d--);
-console.log(5%2);
+// console.log(i++);
+// console.log(d--);
+// console.log(5%2);
 
-console.log("2" == 2);
-console.log("2" === 2);
+// console.log("2" == 2);
+// console.log("2" === 2);
 
-let isChecked = false,
-    isClose = true;
+// let isChecked = false,
+//     isClose = true;
     
-console.log(isChecked || isClose);
+// console.log(isChecked || isClose);
 
-console.log(!isChecked);
+// console.log(!isChecked);
 
-if (4 + 4 == 8) {
-    console.log('ok');
-} else {
-    console.log('not ok');
-};
+// if (4 + 4 == 8) {
+//     console.log('ok');
+// } else {
+//     console.log('not ok');
+// };
     
 
-let num = 50;
+// let num = 50;
 
-if (num < 49) {
-    console.log('smaller than 49');
-} else if (num > 100) {
-    console.log('more than 100');
-} else {
-    console.log('ok');
-};
-
-(num == 50) ? console.log('ok') : console.log('not ok');
-
-switch (num) {
-    case num < 49:
-    console.log('smaller than 49');
-    break;
-    case num < 100:
-    console.log('more than 100');
-    break;
-    case num < 80:
-    console.log('more than 80');
-    break;
-    case 50:
-    console.log('equal 50');
-    break;
-    default: 
-    console.log('smth wrong');
-    break;
-};
-
-
-let numb = 0;
-// while (numb < 10) {
-//     console.log(numb);
-//     numb++;
+// if (num < 49) {
+//     console.log('smaller than 49');
+// } else if (num > 100) {
+//     console.log('more than 100');
+// } else {
+//     console.log('ok');
 // };
 
-// do {
-//     console.log(numb);
-//     numb++;
-// } while (numb <= 15);
+// (num == 50) ? console.log('ok') : console.log('not ok');
 
-// for (let i = 0; i <= 8; i++) {
-//     console.log(i);
+// switch (num) {
+//     case num < 49:
+//     console.log('smaller than 49');
+//     break;
+//     case num < 100:
+//     console.log('more than 100');
+//     break;
+//     case num < 80:
+//     console.log('more than 80');
+//     break;
+//     case 50:
+//     console.log('equal 50');
+//     break;
+//     default: 
+//     console.log('smth wrong');
+//     break;
 // };
+
+
+// let numb = 0;
+// // while (numb < 10) {
+// //     console.log(numb);
+// //     numb++;
+// // };
+
+// // do {
+// //     console.log(numb);
+// //     numb++;
+// // } while (numb <= 15);
+
+// // for (let i = 0; i <= 8; i++) {
+// //     console.log(i);
+// // };
+
+// // for (let i = 0; i <= 8; i++) {
+// //     if (i == 5) {
+// //         console.log(i);
+// //         break;
+// //     };
+// //     console.log(i);
+// // };
 
 // for (let i = 0; i <= 8; i++) {
 //     if (i == 5) {
-//         console.log(i);
-//         break;
+//         continue;
 //     };
 //     console.log(i);
 // };
 
-for (let i = 0; i <= 8; i++) {
-    if (i == 5) {
-        continue;
-    };
-    console.log(i);
+// function showText (text) {
+//     alert(text);
+// };
+
+// showText('Hello World!');
+
+// function learn (lang, callback) {
+//     console.log('I learn ' + lang);
+//     callback();
+// };
+
+// learn('JavaScript', function() {
+//     console.log('i`m good');
+// });
+
+let user = {
+    name: 'sergey',
+    age: 22,
+    nationality: 'Ukrainian'
 };
+
+console.log(user.age);
+user.born = 1996;
+
+delete user.nationality;
+user.other = {
+    gender: 'male',
+    skin: 'white'
+};
+console.log(user);
+
+for (let key in user) {
+    console.log('This key ' + key + ' has a value ' + user[key]);
+};
+
+console.log(Object.keys(user).length);
--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,6 +1,20 @@
-for (let i = 0; i <= 8; i++) {
-    if (i == 5) {
-        continue;
-    };
-    console.log(i);
+
+let user = {
+    name: 'sergey',
+    age: 22,
+    nationality: 'Ukrainian'
+};
+
+console.log(user.age);
+user.born = 1996;
+
+delete user.nationality;
+user.other = {
+    gender: 'male',
+    skin: 'white'
+};
+console.log(user);
+
+for (let key in user) {
+    console.log('This key ' + key + ' has a value ' + user[key]);
 };
